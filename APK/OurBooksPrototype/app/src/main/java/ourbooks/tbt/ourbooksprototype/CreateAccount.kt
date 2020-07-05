cb6160ea2bb43e5ebbd09476cbc08cebddb5dcf9
tbeet <thebesttechnology2020@gmail.com>
2020-07-04 21:44:34 -0500

    YA CREA CUENTA DESDE APK CONSUMIENDO API REST

--- APK/OurBooksPrototype/app/src/main/java/ourbooks/tbt/ourbooksprototype/CreateAccount.kt
+++ APK/OurBooksPrototype/app/src/main/java/ourbooks/tbt/ourbooksprototype/CreateAccount.kt
@@ -1,27 +1,52 @@
 package ourbooks.tbt.ourbooksprototype
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_create_account.*
 import org.json.JSONObject
+import java.text.FieldPosition
+import java.time.LocalDateTime
 
 class CreateAccount : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
 
+        var pais: String = ""
+        spinnerCountrys.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (parent != null) {
+                    pais = parent.getItemAtPosition(position).toString()
+                }
+            }
+        }
 
 
+        btnCreatAccount.setOnClickListener(){
+            val  nombreUsuarioPerfil = usrNamePerf.text.toString()
+            val  nombres = usrNames.text.toString()
+            val  apellidos = usrLastNames.text.toString()
+            val  correo = mail.text.toString()
+            val  contrasena = pswrdd.text.toString()
+            val fechaCreacion = LocalDateTime.now().toString()
 
-    //        val adapter = ArrayAdapter(this, 0,0)
-        //countrys
+            crearCuenta(nombreUsuarioPerfil,nombres,apellidos,correo,pais,fechaCreacion, contrasena)
+        }
     }
 
-    fun crearCuenta(){
+
+    fun crearCuenta(nombreUsuario:String,nombres:String,apellidos:String,correo:String,pais:String,fechaCreacion:String,claveCuenta:String){
 
         //crear cola de requests
         val queue = Volley.newRequestQueue(this)
@@ -31,25 +56,32 @@ class CreateAccount : AppCompatActivity() {
 
         //opjeto json que se va a enviar al api rest
         val jsonObjectCrearCuenta = JSONObject()
-        jsonObjectCrearCuenta.put("nombreUsuario","Lorena925")
-        jsonObjectCrearCuenta.put("nombres","Angie Lorena")
-        jsonObjectCrearCuenta.put("apellidos","Gonzales Perez")
-        jsonObjectCrearCuenta.put("correo","angie@gmai.com")
-        jsonObjectCrearCuenta.put("pais","Francia")
-        jsonObjectCrearCuenta.put("fechaCreacion","2019-06-25")
-        jsonObjectCrearCuenta.put("contrasena","123")
+        jsonObjectCrearCuenta.put("nombreUsuario",nombreUsuario)
+        jsonObjectCrearCuenta.put("nombres",nombres)
+        jsonObjectCrearCuenta.put("apellidos",apellidos)
+        jsonObjectCrearCuenta.put("correo",correo)
+        jsonObjectCrearCuenta.put("pais",pais)
+        jsonObjectCrearCuenta.put("fechaCreacion",fechaCreacion)
+        jsonObjectCrearCuenta.put("contrasena",claveCuenta)
 
         //realizar la peticion post
         val jsonObjectRequest = JsonObjectRequest(url, jsonObjectCrearCuenta,
             Response.Listener { response ->
-                println("Respuesta = $response")
+                //println("Respuesta = $response")
+                msgToast("Cuenta Creada Exitosamentr")
+
             },
             Response.ErrorListener { error ->
                 error.printStackTrace()
-                println("Error al crear cuenta")
+                msgToast("Error al crear cuenta")
             }
         )
         //agregar el request a la cola de requests
         queue.add(jsonObjectRequest)
+    }
+
+
+    fun msgToast(msj: String){
+        val toast = Toast.makeText(this,msj,Toast.LENGTH_LONG).show()
     }
 }
